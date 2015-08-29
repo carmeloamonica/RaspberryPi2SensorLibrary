a83f0a63818319dc56cd96d83f19b0b602ea80cb
CARMELO LA MONICA <carmelolm78@live.it>
2015-08-29 21:24:52 +0200

    Aggiornamento

--- AutomaticFlashingColorfulLEDModule/MainPage.xaml.cs
+++ AutomaticFlashingColorfulLEDModule/MainPage.xaml.cs
@@ -8,7 +8,7 @@ using Windows.UI.Xaml.Navigation;
 
 // Il modello di elemento per la pagina vuota è documentato all'indirizzo http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
 
-namespace RelayModule
+namespace AutomaticFlashingColorfulLEDModule
 {
     /// <summary>
     /// Pagina vuota che può essere utilizzata autonomamente oppure esplorata all'interno di un frame.
@@ -17,7 +17,7 @@ namespace RelayModule
     {
         GPIO _gpio = new GPIO();
         DispatcherTimer _timer = new DispatcherTimer();
-        int _relaymodule = 5; // define the tilt switch sensor interfaces
+        int _automaticFlashingColorfulLEDModule = 5; // define the tilt switch sensor interfaces
         int _val = 0;// define numeric variables val
 
 
@@ -28,9 +28,9 @@ namespace RelayModule
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            _gpio.InitGPIO(_relaymodule);
+            _gpio.InitGPIO(_automaticFlashingColorfulLEDModule);
             SetSensor();
-            _timer.Interval = TimeSpan.FromMilliseconds(0.1);
+            _timer.Interval = TimeSpan.FromMilliseconds(1);
             _timer.Tick += Timer_Tick;
             _timer.Start();
         }
@@ -49,9 +49,9 @@ namespace RelayModule
         private void ReadVal()
         {
             _gpio._pin[0].Write(GpioPinValue.High);
-            Task.Delay(2000).Wait();
+            Task.Delay(3000).Wait();
             _gpio._pin[0].Write(GpioPinValue.Low);
-            Task.Delay(2000).Wait();
-        }      
+            Task.Delay(1000).Wait();
+        }
     }
 }
