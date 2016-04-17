1b716a56f1bd0e4e0c05be4b0507541814d53def
CARMELO LA MONICA <carmelolm78@live.it>
2016-04-17 16:06:25 +0200

    Nuovo progetto ThumbJoystick

---
+++ Thumb Joystick/MCP3008.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Globalization;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Spi;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace Thumb_Joystick
+{
+    public class Mcp3008
+    {
+        SpiDevice _device;
+        string _serialcomunication;
+        string _choicechannel;
+        const int Resolutionbits = 10;
+        const int Shiftbyte = 8;
+
+
+        byte[] _ch0;
+        byte[] _ch1;
+        byte[] _ch2;
+        byte[] _ch3;
+        byte[] _ch4;
+        byte[] _ch5;
+        byte[] _ch6;
+        byte[] _ch7;
+        byte[] _datareceived = {0, 0, 0};
+        byte[] _datareceived1 = { 0, 0, 0 };
+
+
+        public async void InitializeMcp3008(SerialComunication serialcomunication, SpiComunication spicomunication, SpiMode mode)
+        {
+            _serialcomunication = serialcomunication.ToString();           
+            var spiconnectionsettings = new SpiConnectionSettings((int) spicomunication)
+            {
+                ClockFrequency = 1650000,
+                Mode = mode
+            };
+
+
+            var spiDevice = SpiDevice.GetDeviceSelector(spicomunication.ToString());
+            var deviceInformation = await DeviceInformation.FindAllAsync(spiDevice);
+
+
+            if (deviceInformation != null && deviceInformation.Count > 0)
+            {
+                _device = await SpiDevice.FromIdAsync(deviceInformation[0].Id, spiconnectionsettings);
+            }
+
+            else
+            {
+                var dialog = new MessageDialog("Device not found");
+                await dialog.ShowAsync();
+            }
+        }
+
+        public double SingleEndedResult(Channel channel)
+        {
+            _choicechannel = channel.ToString();
+            _ch0 = new byte[] { 1, 0x80, 0 };
+            _ch1 = new byte[] { 1, 0x90, 0 };
+            _ch2 = new byte[] { 1, 0xA0, 0 };
+            _ch3 = new byte[] { 1, 0xB0, 0 };
+            _ch4 = new byte[] { 1, 0xC0, 0 };
+            _ch5 = new byte[] { 1, 0xD0, 0 };
+            _ch6 = new byte[] { 1, 0xE0, 0 };
+            _ch7 = new byte[] { 1, 0xF0, 0 };
+
+            switch (_choicechannel)
+            {
+                case "Ch0":
+                    _device.TransferFullDuplex(_ch0, _datareceived);
+                    break;
+
+                case "Ch1":
+                    _device.TransferFullDuplex(_ch1, _datareceived);
+                    break;
+
+                case "Ch2":
+                    _device.TransferFullDuplex(_ch2, _datareceived);
+                    break;
+
+                case "Ch3":
+                    _device.TransferFullDuplex(_ch3, _datareceived);
+                    break;
+
+                case "Ch4":
+                    _device.TransferFullDuplex(_ch4, _datareceived);
+                    break;
+
+                case "Ch5":
+                    _device.TransferFullDuplex(_ch5, _datareceived);
+                    break;
+
+                case "Ch6":
+                    _device.TransferFullDuplex(_ch6, _datareceived);
+                    break;
+
+                case "Ch7":
+                    _device.TransferFullDuplex(_ch7, _datareceived);
+                    break;
+            }
+
+            var result = ((_datareceived[1] & 0x03) << Shiftbyte) + _datareceived[2];
+            var ohm = result / Resolutionbits;
+
+            //var Ry = (float)(1023 - _DATARECEIVED[0]) * 10 / _DATARECEIVED[0];
+            return ohm;
+        }
+
+        public void DifferentialResult(params TextBlock []textBlocks)
+        {
+            double[] result = {0, 0};
+            double[] ohm = {0, 0};
+
+            _ch0 = new byte[] { 1, 0x80, 0 };
+            _ch1 = new byte[] { 1, 0x90, 0 };
+
+            _device.TransferFullDuplex(_ch0, _datareceived);
+            _device.TransferFullDuplex(_ch1, _datareceived1);
+
+            result[0] = ((_datareceived[1] & 0x03) << Shiftbyte) + _datareceived[2];
+            result[1] = ((_datareceived1[1] & 0x03) << Shiftbyte) + _datareceived1[2];
+
+            ohm[0] = result[0] / Resolutionbits;
+            ohm[1] = result[1] / Resolutionbits;
+
+            textBlocks[0].Text = ohm[0].ToString(CultureInfo.InvariantCulture);
+            textBlocks[1].Text = ohm[1].ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
+
+    public enum SerialComunication
+    {
+        SingleEnded,
+        Differential
+    }
+
+    public enum Channel
+    {
+        Ch0,
+        Ch1,
+        Ch2,
+        Ch3,
+        Ch4,
+        Ch5,
+        Ch6,
+        Ch7
+    }
+
+    public enum SpiComunication
+    {
+        Spi0,
+        Spi1
+    }
---
+++ Thumb Joystick/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Windows.Devices.Gpio;
+using Windows.Devices.Spi;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using GpioConfiguration;
+
+// Il modello di elemento per la pagina vuota è documentato all'indirizzo http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
+
+namespace Thumb_Joystick
+{
+    /// <summary>
+    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        readonly GPIO _gpio = new GPIO();        
+        readonly Mcp3008 _mcp3008 = new Mcp3008();
+        readonly int _switchJoystick = 5; // define the tilt switch sensor interfaces
+        int _val;
+
+        public MainPage()
+        {              
+            InitializeComponent();
+            var timer = new DispatcherTimer();
+            _mcp3008.InitializeMcp3008(SerialComunication.SingleEnded,  SpiComunication.Spi0,  SpiMode.Mode0);
+            _gpio.InitGPIO(_switchJoystick);
+            SetSensor();
+            timer.Interval = new TimeSpan(0, 0,0,0,1000);
+            timer.Start();
+            timer.Tick += _timer_Tick;
+        }
+
+        void _timer_Tick(object sender, object e)
+        {
+            ReadVal();
+            //XCoordinate.Text = Math.Round(_mcp3008.SingleEndedResult(Channel.Ch0)).ToString(CultureInfo.InvariantCulture);
+            _mcp3008.DifferentialResult(XCoordinate, YCoordinate);
+        }
+
+        private void SetSensor()
+        {
+            _gpio._pin[0].SetDriveMode(GpioPinDriveMode.Input);            
+        }
+
+
+        void ReadVal()
+        {
+            // digital interface will be assigned a value of 3 to read val
+            _val = (int)_gpio._pin[0].Read();
+
+            //When the tilt sensor detects a signal when the switch, LED flashes
+            TblSwitch.Text = _val.Equals((int)GpioPinValue.Low) ? "Switch button pressed" : "Switch button unpressed";
+        }
+    }
+}
